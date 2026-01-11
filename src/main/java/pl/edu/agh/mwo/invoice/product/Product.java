14490a82c70b1c42a2739d24fd28cec3b60f7ddb
Klaudia Ścigalska <kscigalska@student.agh.edu.pl>
2026-01-11 14:17:56 +0100

    "all test price done"

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,6 +10,17 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException("name cannot be null");
+        }
+
+        if (price == null){
+            throw new IllegalArgumentException("price cannot be null");
+        }
+        if (price.compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException("price cannot be less than 0");
+        }
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
